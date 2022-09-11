6ae7656bb624ae6bbe3f8d36be822be0b722738f
Satyam-Modi <satyammodi0402@gmail.com>
2022-09-11 12:58:36 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { AppBar, Tab, Tabs, Toolbar, Typography } from '@mui/material';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import { NavLink } from 'react-router-dom';
+const Header = () => {
+    const [value, setValue] = useState();
+    return (
+        <div>
+            <AppBar sx={{backgroundColor: '#232F3D'}} position='stick'>
+                <Toolbar>
+                    <NavLink to="/" style={{ color: "white" }}>
+                        <Typography>
+                            <LibraryBooksOutlinedIcon/>
+                        </Typography> 
+                    </NavLink>
+                    <Tabs sx={{ml:'auto'}} textColor='inherit' indicatorColor="primary" value={value} onChange={(e,val)=>setValue(val)}>
+                        <Tab LinkComponent={NavLink} to="/add" label="ADD PRODUCT" />
+                        <Tab LinkComponent={NavLink} to="/books" label="Books" />
+                        <Tab LinkComponent={NavLink} to="/about" label="ABOUT US" />
+                    </Tabs>
+                </Toolbar>   
+            </AppBar>
+        </div>
+    );
+};
+
+export default Header;
